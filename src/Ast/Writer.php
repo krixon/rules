95dc580e6b59b57c02aa1bf315dc7eefde8efbe2
karl.rixon <karl.rixon@maruedr.com>
2018-12-04 12:26:50 +0000

    Add an AST writer to turn AST back into source code

---
+++ src/Ast/Writer.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Krixon\Rules\Ast;
+
+class Writer implements Visitor
+{
+    private $buffer;
+
+
+    public function write(Node $node) : string
+    {
+        $this->buffer = '';
+
+        $node->accept($this);
+
+        return trim($this->buffer);
+    }
+
+
+    public function visitIdentifier(IdentifierNode $node) : void
+    {
+        $this->buffer .= $node->fullName();
+    }
+
+
+    public function visitNodeList(NodeList $node) : void
+    {
+        $this->buffer .= '[';
+
+        foreach ($node->nodes() as $child) {
+            $child->accept($this);
+            $this->buffer .= ', ';
+        }
+
+        $this->buffer = trim($this->buffer, ', ') . ']';
+    }
+
+
+    public function visitLogical(LogicalNode $node) : void
+    {
+        $this->buffer .= '(';
+
+        $node->left()->accept($this);
+
+        $this->buffer .= $node->isAnd() ? ' and ' : ' or ';
+
+        $node->right()->accept($this);
+
+        $this->buffer .= ')';
+    }
+
+
+    public function visitComparison(ComparisonNode $node) : void
+    {
+        $node->left()->accept($this);
+
+        if ($node->isEqual()) {
+            $this->buffer .= ' is ';
+        } elseif ($node->isNotEqual()) {
+            $this->buffer .= ' not ';
+        } else {
+            $this->buffer .= ' in ';
+        }
+
+        $node->right()->accept($this);
+    }
+
+
+    public function visitString(StringNode $node) : void
+    {
+        $this->buffer .= '"' . $node->value() . '"';
+    }
+
+
+    public function visitNumber(NumberNode $node) : void
+    {
+        $this->buffer .= $node->value();
+    }
+}
---
+++ tests/Functional/Ast/WriterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Krixon\Rules\Tests\Functional\Ast;
+
+use Krixon\Rules\Ast\Writer;
+use Krixon\Rules\ExpressionParser;
+use PHPUnit\Framework\TestCase;
+
+class WriterTest extends TestCase
+{
+    /**
+     * @dataProvider expectedStringProvider
+     *
+     * @param string $expression
+     * @param string $expected
+     *
+     * @throws \Krixon\Rules\Exception\SyntaxError
+     */
+    public function testProducesExpectedString(string $expression, string $expected = null) : void
+    {
+        if (null === $expected) {
+            $expected = $expression;
+        }
+
+        $ast = (new ExpressionParser())->parse($expression);
+
+        static::assertSame($expected, (new Writer())->write($ast));
+    }
+
+
+    public function expectedStringProvider() : array
+    {
+        return [
+            ['foo is 42'],
+            ['foo is "bar"'],
+            ['foo not "bar"'],
+            [
+                'foo is 42 and bar is 666',
+                '(foo is 42 and bar is 666)',
+            ],
+            [
+                '(foo is 42 and bar is 666) or bar is 667',
+                '((foo is 42 and bar is 666) or bar is 667)',
+            ],
+            [
+                'foo is 42 and (bar is 666 or bar is 667)',
+                '(foo is 42 and (bar is 666 or bar is 667))',
+            ],
+        ];
+    }
+}
